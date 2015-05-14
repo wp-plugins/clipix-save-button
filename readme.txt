d9c5368e8bc145db8c94c09c5bf9dd2e538727ed
markluo <markluo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 20:31:08 +0000

    added customization options
    git-svn-id: https://plugins.svn.wordpress.org/clipix-save-button/trunk@1160752 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Clipix.com
 Tags: clipix, save button, save
 Requires at least: 3.9
 Tested up to: 4.2
-Stable tag: 0.1.0
+Stable tag: 0.1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -11,9 +11,9 @@ Add a Clipix "Save" Button to your site and get your visitors to save and come b
 
 == Description ==
 
-This plug in lets you easily add a "Save" button to your website, for articles, products or content. Once a visitor has saved your content or products they'll be much more likely to come back! Simply install the plugin, and choose where you'd like it to appear. 
+No need to copy and paste code throughout your WordPress site.
 
-Clipix.com: Save everything you care about, organized in one place, accessible from anywhere.
+Add Clipix "Save" Button to your posts in minutes!
 
 == Installation ==
 
@@ -41,11 +41,10 @@ Download & Upload
 
 Clipix is an unbelievably useful tool that lets you save and organize everything you see online, and offline, into one central place. Everything you save is called a "clip". Clips are placed onto your personal clipboards for anything imaginable; articles you want to read, gift ideas, comparison shopping, home decorating &ndash; the possibilities are literally endless. It's free, fun, and changing the way we organize our lives!
 
-Questions? Need Assistance? 
-
-Contact Us: "devsupport@clipix.com"
-
 == Changelog ==
+
+= 0.1.1 =
+* Added options to customize the Save button
 
 = 0.1.0 =
 * Initial release
--- savebutton.php
+++ savebutton.php
@@ -2,20 +2,31 @@
 /*
 Plugin Name: Clipix "Save" Button
 Plugin URL: http://www.clipix.com
-Version: 0.1.0
+Version: 0.1.1
 Author: clipix.com
 Author URL: http://www.clipix.com
 Description: The "Save" Button powered by Clipix.com
 */
 register_activation_hook( __FILE__, 'clipix_activate' );
 add_action('admin_menu', 'Clipix_plugin_setup_menu');
-function clipix_activate() {
+wp_register_style( 'clipix_admin', plugins_url( 'css/admin.css', __FILE__ )  );
 
+function clipix_activate() {
+		$clipix_options = get_option('clipix_setting'); 
+	
 		$defaults = array(
 		'clipix_position_ckb_top' => '1',
+		'clipix_position_ckb_bottom' => '0',
+		'clipix_size' => 'medium',
+		'clipix_color' => 'gray',
+		'language' => 'en'
 		);
-	add_option('clipix_setting', $defaults);
-
+		foreach ( $defaults as $k => $v )
+    {
+        $clipix_options[$k]=$v;
+    }
+	/*$clipix_options = get_option('clipix_setting', $defaults);*/
+	
 
 }
 
@@ -35,9 +46,11 @@ function Clipix_settings() {
 function clipix_validate($input) {
   return array_map('wp_filter_nohtml_kses', (array)$input);
 } 
-function Clipix_plugin_setup_init(){ ?>
-<div>
-  <h2>Clipix "Save" Button position</h2>
+function Clipix_plugin_setup_init(){ 
+wp_enqueue_style('clipix_admin'); ?>
+<div class="wrap">
+<div class="clipix-settings-content">
+ <h2><img src="<?php echo plugin_dir_url( __FILE__ ); ?>assets/clipix_logo32.png" style="vertical-align: bottom;" /> Clipix "Save" Button Settings</h2>
   <form method="post" action="options.php">
   <?php
   settings_fields('clipix_options_group');
@@ -46,25 +59,80 @@ function Clipix_plugin_setup_init(){ ?>
 
   
   ?>
-<table>   
+<table class="form-table">   
    <tbody>
 
    <tr>
       <th scope="row" style="vertical-align: initial;   width: 200px; text-align: left;">Button Placement</th>
       <td>
-         <label for="clipix_setting[clipix_position_ckb_top]" class="pib-checkbox-label">
+         <label for="clipix_setting[clipix_position_ckb_top]" class="clipix-checkbox-label">
 		 <input name="clipix_setting[clipix_position_ckb_top]" id="clipix_setting[clipix_position_ckb_top]" type="checkbox" value="1" <?php checked('1', $clipix_options['clipix_position_ckb_top']); ?> />
-         Above Content</label><br/>
-		 <label for="clipix_setting[clipix_position_ckb_bottom]" class="pib-checkbox-label">
+         Above Content</label>
+		 <label for="clipix_setting[clipix_position_ckb_bottom]" class="clipix-checkbox-label">
 		 <input name="clipix_setting[clipix_position_ckb_bottom]" id="clipix_setting[clipix_position_ckb_bottom]" type="checkbox" value="1" <?php checked('1', $clipix_options['clipix_position_ckb_bottom']); ?> />
          Below Content</label>
 		
+      </td>
+   </tr>  
+   <tr> 
+      <th scope="row" style="vertical-align: initial;   width: 200px; text-align: left;">Size</th>
+      <td>
+        <label for="clipix_setting[clipix_size][small]" class="clipix-radio-label">
+      	<input  type="radio" name="clipix_setting[clipix_size]" value="small" <?php checked('small', $clipix_options['clipix_size']); ?> />Small (20px)</label>
+  		<label for="clipix_setting[clipix_size][medium]" class="clipix-radio-label">
+  		<input type="radio" name="clipix_setting[clipix_size]" value="medium" <?php checked('medium', $clipix_options['clipix_size']); ?> />Medium (24px)</label> 
+  		<label for="clipix_setting[clipix_size][large]" class="clipix-radio-label">
+  		<input type="radio" name="clipix_setting[clipix_size]" value="large" <?php checked('large', $clipix_options['clipix_size']); ?> />Large (32px)<br /></label> 
+   </tr>
+   <tr> 
+      <th scope="row" style="vertical-align: initial;   width: 200px; text-align: left;">Color Style</th>
+      <td>
+      	<label for="clipix_setting[clipix_color][gray]" class="clipix-radio-label">
+      	<input type="radio" name="clipix_setting[clipix_color]" value="gray" <?php checked('gray', $clipix_options['clipix_color']); ?> />Gray</label>
+      	<label for="clipix_setting[clipix_color][white]" class="clipix-radio-label">
+  		<input type="radio" name="clipix_setting[clipix_color]" value="white" <?php checked('white', $clipix_options['clipix_color']); ?> />White</label>  
+  		<label for="clipix_setting[clipix_color][orange]" class="clipix-radio-label">
+  		<input type="radio" name="clipix_setting[clipix_color]" value="orange" <?php checked('orange', $clipix_options['clipix_color']); ?> />Orange</label>
+      </td>
+   </tr>
+      <tr> 
+      <th scope="row" style="vertical-align: initial;   width: 200px; text-align: left;">Language</th>
+      <td>
+<select name="clipix_setting[language]" id="clipix_setting[language]"  >
+<?php 
+    $language = array(
+        'en' => 'English',
+        'es' => 'español',
+        'fr' => 'français',
+        'he' => 'עברית',
+        'zh' => '中文',
+        'it' => 'italiano',
+        'tr' => 'Türkçe',
+        'de' => 'Deutsch',
+        'pt' => 'Português',
+        'ko' => '한국어',
+        'ja' => '日本語',
+        'ru' => 'русский'
+        
+    );
+    $selectedLan = esc_attr( $language );
+    foreach ($language as $code => $label) {
+        echo '<option value="'.$code.'"';
+        if ($selectedLan == $code||$clipix_options['language']== $code) {
+        	$clipix_options['language'] = $selectedLan ;
+            echo ' selected="selected"';
+        }
+        echo '>' . $label . '</option>';
+    }
+?>
+</select>
       </td>
    </tr>
 </tbody>
 </table>     
   <?php submit_button(); ?>
   </form>
+</div>
 </div>
 <?php } 
 
@@ -82,7 +150,7 @@ class Add_Clipix_Save_Button {
 	{
 		/*template file */
 		$fileName = dirname(__FILE__) ."/Clipix_Save_Button_Template.txt";
-		
+		$result = $content;
 		if(is_single() && !is_page( ) && file_exists( $fileName )){
 
 			/*open the template file and read */
@@ -92,7 +160,60 @@ class Add_Clipix_Save_Button {
 			$clipix_options = get_option('clipix_setting');
 			$positionTop=$clipix_options['clipix_position_ckb_top'];
 			$positionBottom=$clipix_options['clipix_position_ckb_bottom'];
-			$result = $content;
+			$color = $clipix_options['clipix_color'];
+			$size = $clipix_options['clipix_size'];
+			
+						
+			if ($msg != NULL)
+			{
+
+				$msg = str_replace("{ln}", $clipix_options['language'], $msg);
+				
+				switch ($color) {
+				    case "gray":
+				    	switch ($size)
+				    		{
+				    			case "small":
+				    			$msg = str_replace("{opt}", 1, $msg);
+				    			break;
+				    			case "medium":
+				    			$msg = str_replace("{opt}", 2, $msg);
+				    			break;
+				    			case "large":
+				    			$msg = str_replace("{opt}", 3, $msg);
+				    			break;							
+							}
+				        break;
+				    case "orange":
+				    	switch ($size)
+				    		{
+				    			case "small":
+				    			$msg = str_replace("{opt}", 4, $msg);
+				    			break;
+				    			case "medium":
+				    			$msg = str_replace("{opt}", 5, $msg);
+				    			break;
+				    			case "large":
+				    			$msg = str_replace("{opt}", 6, $msg);
+				    			break;							
+							}
+				        break;
+				   case "white":
+				    	switch ($size)
+				    		{
+				    			case "small":
+				    			$msg = str_replace("{opt}", 7, $msg);
+				    			break;
+				    			case "medium":
+				    			$msg = str_replace("{opt}", 8, $msg);
+				    			break;
+				    			case "large":
+				    			$msg = str_replace("{opt}", 9, $msg);
+				    			break;							
+							}
+				        break;
+				}
+				
 			if( $positionTop == '1')
 			{
 				$result = stripslashes( $msg ) . $content;
@@ -104,11 +225,12 @@ class Add_Clipix_Save_Button {
 				$result = $result . stripslashes( $msg );
 			}
 		}
-			return $result;
+			
 		
 	}
+	return $result;
 }
-
+}
 $_addClipixSaveButton = new Add_Clipix_Save_Button();
 
 ?>
